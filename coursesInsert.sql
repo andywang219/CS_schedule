2bb5097383088469345eb038b49361d651d8e6f1
Andy Wang <andywang219@yahoo.com>
2018-03-16 02:18:14 -0400

    Create coursesInsert.sql

---
+++ coursesInsert.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+  courses(course_num, courseName, professors_id, credits, prereqs)
+VALUES
+  (10400, 'Discrete Mathematical Structures I', 3, 4, 'MATH20100'),
+  (10400, 'Discrete Mathematical Structures I', 4, 4, 'MATH20100'),
+  (10400, 'Discrete Mathematical Structures I', 5, 4, 'MATH20100'),
+  (11300, 'Programming Language', 6, 1, 'CSC10300');
