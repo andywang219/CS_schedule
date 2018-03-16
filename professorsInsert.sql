4aaa19c6f4d3eab5fcd620f349f99ca7986bdabd
Andy Wang <andywang219@yahoo.com>
2018-03-16 01:05:47 -0400

    Update professorsInsert.sql

--- professorsInsert.sql
+++ professorsInsert.sql
@@ -1,2 +1,31 @@
 INSERT INTO professors(prof_fname, prof_lname) VALUES
 ('prof_fname', 'prof_lname');
+
+INSERT INTO professors
+   (prof_fname, prof_lname)
+VALUES
+    ('William', 'Skeith'),
+   	('Jeffrey', 'Gitlin'),
+   	('Peter', 'Brass'),
+   	('Michael', 'Vulis'),
+   	('Douglas', 'Troeger'),
+   	('Ahmet', 'Yuksel'),
+   	('Isidor', 'Gertner'),
+   	('Xiang', 'Meng'),
+   	('Leonid', 'Gurtvis'),
+   	('Irina', 'Gladkova'),
+   	('Jie', 'Wei'),
+   	('Zheng', 'Peng'),
+   	('Donald', 'Gordon'),
+   	('Stephen', 'Lucci'),
+   	('Mohammed', 'Shibli'),
+   	('John', 'Connor'),
+   	('Maryam', 'Vatankhah'),
+   	('Nooredin', 'Naghibolhosseini'),
+    ('Cesar', 'Rojas'),
+    ('Zhigang', 'Zhu'),
+    ('Elahe', 'Vahdani'),
+    ('See', 'Chiu'),
+    ('Jie', 'Wei')
+    ('Devendra', 'Kumar'),
+    ('Peter', 'Barnett');
