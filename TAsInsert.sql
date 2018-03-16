c3bc7cea89de00b7007e9442a50163d7a27b4a94
Andy Wang <andywang219@yahoo.com>
2018-03-16 02:18:59 -0400

    Create TAsInsert.sql

---
+++ TAsInsert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+  TAs(ta_fname, ta_lname, course_section)
+VALUES
+  ('Amir', 'Mohsen', 1),
+  ('Toliyat', 'Abolhassani', 1),
+  ('Bin', 'Mu', 2),
+  ('Siyu', 'Liao', 3),
+  ('Nihal', 'Vatandas', 4),
+  ('Chi-Him', 'Liu', 5),
+  ('Isatou', 'Sanneh', ___),
+  ('Michael', 'Iannelli', ___);
