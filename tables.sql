73933c2739a0795f674eee9d1e13881a5a2e3b4f
buenoluis007 <33500965+buenoluis007@users.noreply.github.com>
2018-03-16 00:08:58 -0400

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,25 +1,27 @@
 CREATE TABLE professors(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    prof_fname VARCHAR(100),
-    prof_lname VARCHAR(100),
-    department VARCHAR(100)
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+prof_fname VARCHAR(100) NOT NULL,
+prof_lname VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE course(
-    code INT PRIMARY KEY NOT NULL,
-    courseNAME VARCHAR(255),
+CREATE TABLE courses(
+section INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    course_num INT NOT NULL, -- example: 22000
+    courseName VARCHAR(50) NOT NULL, -- example: algorithm
     professors_id INT,
-    seats INT,
+    seats INT NOT NULL,
     credits INT NOT NULL,
-    FOREIGN KEY(professors_id) REFERENCES professors(id)
+    prereqs VARCHAR(250),
+    FOREIGN KEY(professors_id) REFERENCES professors(id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE schedule(
-    course_code INT NOT NULL,
+    course_section INT NOT NULL,
     building VARCHAR(255),
     room VARCHAR(100),
     times VARCHAR(255),
-    FOREIGN KEY(course_code) REFERENCES course(code)
+    FOREIGN KEY(course_section) REFERENCES courses(section) ON DELETE CASCADE
 );
 
 CREATE TABLE ratings(
@@ -28,6 +30,27 @@ CREATE TABLE ratings(
     chilly_pepper VARCHAR(7),
     would_take_again INT,
     professors_id INT,
-    FOREIGN KEY(professors_id) REFERENCES professors(id))
+    FOREIGN KEY(professors_id) REFERENCES professors(id) ON DELETE CASCADE
 );
 -- Ratings will be: not hot, hot, sexy
+
+CREATE TABLE reviews(
+ review VARCHAR(255),
+      created DATE,
+      professor_id INT,
+      FOREIGN KEY(professor_id) REFERENCES professors(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE users(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100)
+);
+
+
+CREATE TABLE TAs(
+ta_fname VARCHAR(20),
+ta_lname VARCHAR(20),
+course_section INT,
+FOREIGN KEY(course_section) REFERENCES courses(section) ON DELETE CASCADE
+);
