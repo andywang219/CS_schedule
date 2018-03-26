45cc962917714dd7ab51da9fd6ed709bcbfe1903
Andy Wang <andywang219@yahoo.com>
2018-03-25 23:57:57 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,18 +1,35 @@
--- Some queries
-SELECT courseName, CONCAT_WS(' ', prof_fname, prof_lname) AS Professor
-FROM courses JOIN professors ON courses.professors_id = professors.id;
-
+-- courses w/ professor w/ rating w/ location and time
 SELECT
-    CONCAT('CSC', course_num,' ', courseName) AS Course,
-    CONCAT_WS(' ', prof_fname, prof_lname) AS Professor,
-    prof_rating,
-    difficulty,
-    would_take_again AS 'Would Take Again (%)',
-    chilly_pepper,
-    CONCAT(SUBSTRING(review, 1, 50), '...') AS 'Recent Reviews',
-    created AS 'Created On'
+       CONCAT("CSC",course_num, " ",courseName) AS Course,
+       CONCAT_WS(" ", prof_fname, prof_lname) AS Professor,
+       CASE
+          WHEN prof_rating = -1 THEN "N/A"
+          ELSE prof_rating
+       END AS "Professor Rating",
+       CONCAT_WS(", ", building, room, times) AS "Location and Time"
 FROM courses JOIN professors ON courses.professors_id = professors.id
              JOIN ratings ON ratings.professors_id = professors.id
-             JOIN reviews ON reviews.professors_id = professors.id
-GROUP BY review
-ORDER BY prof_lname;
+             JOIN schedule ON courses.section = schedule.course_section
+ORDER BY Course;
+
+-- professor w/ ratings/ with review
+SELECT
+    CONCAT_WS(" ", prof_fname, prof_lname) AS Professor,
+    CASE
+       WHEN prof_rating = -1 THEN "N/A"
+       ELSE prof_rating
+    END AS "Professor Rating",
+    CASE
+       WHEN difficulty = -1 THEN "N/A"
+       ELSE difficulty
+    END AS Difficulty,
+    CASE
+       WHEN would_take_again = -1 THEN "N/A"
+       ELSE would_take_again
+    END AS "Would Take Again %",
+    chilly_pepper AS "Popularity",
+    CONCAT(SUBSTRING(review, 1, 30), "...") AS Review,
+    created AS "Created On"
+FROM professors JOIN ratings ON ratings.professors_id = professors.id
+                JOIN reviews ON reviews.professors_id = professors.id
+ORDER BY Professor;
