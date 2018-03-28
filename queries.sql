545d6be7478fd660b62d1f927ad99ba4c93d57a7
Andy Wang <andywang219@yahoo.com>
2018-03-27 23:35:02 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -40,22 +40,27 @@ SELECT
        section AS Section,
        CONCAT("CSc",course_num, " ",courseName) AS Course,
        CONCAT_WS(" ", prof_fname, prof_lname) AS Professor,
-       IFNULL(CONCAT_WS(" ", ta_fname, ta_lname), "None") AS "TA",
+       IFNULL(CONCAT_WS(" ", ta_fname, ta_lname), "N/A") AS "TA",
        CASE
           WHEN prof_rating = -1 THEN "N/A"
           ELSE prof_rating
        END AS "Professor Rating",
-       difficulty AS "Difficulty",
+       CASE
+          WHEN difficulty = -1 THEN "N/A"
+          ELSE difficulty
+       END AS "Difficulty",
        CASE
           WHEN would_take_again = -1 THEN "N/A"
           ELSE would_take_again
        END AS "Would Take Again%",
        chilly_pepper AS "Chilly Pepper",
-       CONCAT(SUBSTRING(review, 1, 10), "...") AS Review,
-       prereq AS "PreReqs",
+       CONCAT(SUBSTRING(review, 1, 30), "...") AS Review,
+       prereqs AS "PreReqs",
        CONCAT_WS(", ", building, room, times) AS "Location and Time"
 FROM courses JOIN professors ON courses.professors_id = professors.id
              JOIN ratings ON ratings.professors_id = professors.id
              JOIN reviews ON reviews.professors_id = professors.id
              JOIN schedule ON courses.section = schedule.course_section
-             JOIN TAs ON courses.section = TAs.course_section;
+             LEFT JOIN TAs ON courses.section = TAs.course_section;
+
+
